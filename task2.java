a7b3e6a92a7dc5093931562ed52a8915c1cf1f5d
Iuliia <ytaratynova@gmail.com>
2023-02-13 15:06:18 +0300

    task2 corrected

--- task2.java
+++ task2.java
@@ -5,8 +5,8 @@ package seminar1_homework;
 public class task2 {
     public static void main(String[] args) {
         for (int i = 2; i <=1000; i++){
-            for (int j = 2; j < i; j++){
-                if (i % j == 0){
+            for (int j = 1; j < i; j++){
+                if (i % j == 0 && j != 1){
                     break;
                 } else {
                     if (i == j + 1){
